f0a67ac59a824d8ea14f12d2d0d59ac06832b2c8
Zoltan Tolnai <tolnaiz@gmail.com>
2016-04-23 12:04:20 +0200

    wildvard domains

---
+++ scripts/serve-wildcard.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+mkdir /etc/nginx/ssl 2>/dev/null
+
+PATH_SSL="/etc/nginx/ssl"
+PATH_KEY="${PATH_SSL}/wildcard.key"
+PATH_CSR="${PATH_SSL}/wildcard.csr"
+PATH_CRT="${PATH_SSL}/wildcard.crt"
+
+if [ ! -f $PATH_KEY ] || [ ! -f $PATH_CSR ] || [ ! -f $PATH_CRT ]
+then
+  openssl genrsa -out "$PATH_KEY" 2048 2>/dev/null
+  openssl req -new -key "$PATH_KEY" -out "$PATH_CSR" -subj "/CN=$1/O=Vagrant/C=UK" 2>/dev/null
+  openssl x509 -req -days 365 -in "$PATH_CSR" -signkey "$PATH_KEY" -out "$PATH_CRT" 2>/dev/null
+fi
+
+block="server {
+    listen ${3:-80};
+    listen ${4:-443} ssl;
+    server_name $1;
+    root \"$2\";
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log off;
+    error_log  /var/log/nginx/wildcard-error.log error;
+
+    sendfile off;
+
+    client_max_body_size 100m;
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+
+    ssl_certificate     /etc/nginx/ssl/wildcard.crt;
+    ssl_certificate_key /etc/nginx/ssl/wildcard.key;
+}
+"
+
+echo "$block" > "/etc/nginx/sites-available/wildcard"
+ln -fs "/etc/nginx/sites-available/wildcard" "/etc/nginx/sites-enabled/wildcard"
+service nginx restart
+service php7.0-fpm restart
